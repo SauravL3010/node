8c4e54f4756181105e6930ffc12baaca0edcfffe
SauravL3010 <slgurhal@uwaterloo.ca>
2022-04-30 23:23:23 -0400

    added get route /test/true

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('welcome to home page');
+    return res.send('welcome to home page');
 })
 
+app.get('/test/:type', (req, res) => {
+    const type = req.params.type
+    if (type !== 'true') {
+        return res.status(400).json({success: false})
+    }
+    return res.send("you are in test")
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("started ..")
